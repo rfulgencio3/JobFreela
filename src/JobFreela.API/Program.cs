5fc7b968b9f6bf7b0ac0d1d7fd6b4bd6efb26ed4
rfulgencio3 <rfulgencio3@gmail.com>
2024-08-09 16:19:24 -0300

    Ajustes connectionString e criação de Migrations

--- src/JobFreela.API/Program.cs
+++ src/JobFreela.API/Program.cs
@@ -25,7 +25,7 @@ builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 builder.Services.AddScoped<ISkillRepository, SkillRepository>();
 
 builder.Services.AddDbContext<JobFreelaDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("JobFreelaCs")));
 
 
 var app = builder.Build();
