6ca39a823ff31211d0aa8b3797b3e3a2479298b4
rfulgencio3 <rfulgencio3@gmail.com>
2024-08-13 16:41:56 -0300

    Implementando método de encript de senha em CreateUserCommand e implementação de métodos em AuthService

--- src/JobFreela.Application/Commands/CreateUser/CreateUserCommandHandler.cs
+++ src/JobFreela.Application/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -1,5 +1,6 @@
 ﻿using JobFreela.Core.Entities;
 using JobFreela.Core.Repositories;
+using JobFreela.Core.Services;
 using MediatR;
 
 namespace JobFreela.Application.Commands.CreateUser;
@@ -7,13 +8,16 @@ namespace JobFreela.Application.Commands.CreateUser;
 public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
 {
     private readonly IUserRepository _repository;
-    public CreateUserCommandHandler(IUserRepository repository)
+    private readonly IAuthService _service;
+    public CreateUserCommandHandler(IUserRepository repository, IAuthService service)
     {
-        _repository = repository;   
+        _repository = repository;
+        _service = service;
     }
     public async Task<int> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
-        var user = new User(request.FullName, request.Email, request.BirthDate, request.Password, request.Role);
+        var passwordHash = _service.ComputeSha256Hash(request.Password);
+        var user = new User(request.FullName, request.Email, request.BirthDate, passwordHash, request.Role);
 
         await _repository.AddAsync(user);
         
